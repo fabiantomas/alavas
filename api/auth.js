4f33f698fce91dff42ba55d19c0487a93c0376ce
tamas.fabian@audi.hu <tamas.fabian@audi.hu>
2026-06-11 11:08:58 +0200

    add sleep scope

--- api/auth.js
+++ api/auth.js
@@ -4,7 +4,7 @@ export default function handler(req, res) {
     response_type: "code",
     client_id: process.env.POLAR_CLIENT_ID,
     redirect_uri: base + "/api/callback",
-    scope: "accesslink.read_all",
+    scope: "accesslink.read_all sleep:read nightly_recharge:read",
   });
   res.redirect(302, "https://flow.polar.com/oauth2/authorization?" + params);
 }
